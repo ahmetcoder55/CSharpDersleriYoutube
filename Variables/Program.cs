cd897a3d2abda4fc084e36e410a2bc7327b738cf
ahmetcoder55 <244322943+ahmetcoder55@users.noreply.github.com>
2026-09-21 20:21:17 +0300

    21.09.2026 3.Ders Sonu

---
+++ Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿//Variables
+//String="Ahmet"
+//İnt=SAYISAL = 1
+//Float=Ondalıklı Sayı Veri Tipi
+//Bool=True False
+string name = "Salih";
+Console.WriteLine(name);
+Console.WriteLine(name+ " 15 yaşında");
+Console.WriteLine(name+" 15 yaşında ve Kodlama Öğreniyor");
